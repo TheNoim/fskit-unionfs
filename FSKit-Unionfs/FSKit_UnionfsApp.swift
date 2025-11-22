8efe5c9185d24f32d1446644d4850b13c7bf9bda
Nils Bergmann <nilsbergmann@noim.io>
2025-11-22 17:59:49 +0100

    Initial Commit

---
+++ FSKit-Unionfs/FSKit_UnionfsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FSKit_UnionfsApp.swift
+//  FSKit-Unionfs
+//
+//  Created by Nils Bergmann on 22.11.25.
+//
+
+import SwiftUI
+
+@main
+struct FSKit_UnionfsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
